a5b72b0f737f2777b78b13aa50c0ea05959a3cf1
Max Kmet <maxkmet01@gmail.com>
2019-11-29 15:26:28 +0200

    Node with HashMap

---
+++ src/main/java/ua/edu/ucu/iterator/Iterator.java
@@ -0,0 +1,4 @@
+package ua.edu.ucu.iterator;
+
+public class Iterator {
+}
---
+++ src/main/java/ua/edu/ucu/utils/IterableTrie.java
@@ -0,0 +1,11 @@
+package ua.edu.ucu.utils;
+
+import ua.edu.ucu.iterator.Iterator;
+
+public class IterableQueue implements IterableCollection{
+
+    @Override
+    public Iterator createIterator() {
+        return null;
+    }
+}
